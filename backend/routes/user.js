2e8169316e52150e5c3f1d6b619e3d719cd97983
Govind Kushwaha <govindkushwaha@Govinds-MacBook-Air.local>
2023-07-04 13:08:33 +0530

    v1.2 - modified LLM

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,24 +5,36 @@ require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
 
 const router = express.Router();
-let finaldata=[];
 
+let finaldata=[];
 router.use(cors());
 
 router.post("/send", async (req, res) => {
   console.log(req.body);
   const EnteredContent=req.body.appliances;
+  // console.log(EnteredContent[0].name);
   const EnteredBillamt=req.body.bill_amt;
 
   const content = `
   Personalized Home Energy Advisor Recommendations:
   
   Energy bills: ${EnteredBillamt} in Rupees 
-  Appliance information: ${EnteredContent}
+  Appliance_Information: ${JSON.stringify(EnteredContent)}
+  Strictly Assume these variable and act accordingly in the entire prompt:
+    saved_money = ${(EnteredBillamt * 3.14 / 10).toFixed(2)}
+    appliance_name = appliance[index].name
+    appliance_age = appliance[index].age in years
+    appliance_powerConsumption = appliance[index].powerConsumption in watt
+    appliance_usagePerDay = appliance[index].usagePerDay in hours
+
   User preferences=> Priority: Cost savings
-  
+
+    For the Appliance_Information iterate through a whole array of object and generates the suggestion according to their object -> appliance_name, appliance_age, appliance_powerConsumption, appliance_usagePerDay for each index.
+
     Please provide personalized recommendations for reducing energy usage and increasing efficiency based on the given information.
-  
+
+    Give all recommendations in 'energy_saving_tips' according to the 'Appliance_Information'.
+
     Output should be only in json format and should strictly follow the following json structure and it is indented for indian citizens
     {
       recommendations: {
@@ -34,9 +46,10 @@ router.post("/send", async (req, res) => {
           "Suggested upgrade or replacement for Appliance 1",
           "Suggested upgrade or replacement for Appliance 2"
         ],
-        estimated_cost_savings: "By implementing these recommendations, you can potentially save [estimated cost savings in rupees] per [specific time period]."
+        estimated_cost_savings: "By implementing these recommendations, you can potentially save [saved_money] in rupees per [specific time period]."
       }
     }
+
   `;
   
   const configuration = new Configuration({
